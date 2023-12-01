53e8735b18068a623d4cfff35fe7ae10926da983
temidee01 <benjamin.temidee@gmail.com>
2023-12-01 01:49:09 -0800

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -20,5 +20,4 @@ if __name__ == "__main__":
             print("{} / {} = {}".format(a, b, div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
-)
+            sys.exit(1))
